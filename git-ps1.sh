2951a765e675b90d50f98f8f721c30c09f25f983
Julien C <julien.castelain@liferay.com>
2017-05-31 09:04:23 +0200

    Add git prompt

---
+++ git-ps1.sh
@@ -0,0 +1,12 @@
+__git_ps2() {
+  local exit=$?
+  local repo=${PWD##*/}
+
+  if ! git rev-parse --is-inside-work-tree &>/dev/null; then
+    return 0;
+  fi
+
+  local branch="$(git rev-parse --abbrev-ref HEAD)" || branch="unnamed branch"
+  local gitstring=" [$branch]"
+  printf -- "%s" "$gitstring"
+}
