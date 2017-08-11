c0cbb329b85adaf091a2a25bc23dfc280a49e2de
Julien C <julien.castelain@liferay.com>
2017-08-11 20:12:10 +0200

    Update git "prompt" script

--- git-ps1.sh
+++ git-ps1.sh
@@ -7,7 +7,17 @@ __git_ps2() {
     exit;
   fi
 
-  local branch="$(git rev-parse --abbrev-ref HEAD)" || branch="unnamed branch"
+  # local branch="$(git rev-parse --abbrev-ref HEAD)" || branch="unnamed branch"
+
+  local branch=""
+  if git rev-parse --abbrev-ref HEAD &>/dev/null; then
+    branch="$(git rev-parse --abbrev-ref HEAD)"
+  else
+    return 0;
+    exit;
+  fi
+
   local gitstring=" [$branch]"
+
   printf -- "%s" "$gitstring"
 }
