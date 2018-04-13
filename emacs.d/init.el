526b3a63b1564441019a24764bc5698b58ee2c6d
Julien Castelain <julien.castelain@liferay.com>
2018-04-13 20:03:24 +0200

    Update emacs stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,6 +3,16 @@
 (set-keyboard-coding-system nil)
 (setq auto-save-default nil)
 
+
+(cond
+  ((string-equal system-type "darwin")
+    (progn
+      (setq mac-option-key-is-meta nil)
+      (setq mac-command-key-is-meta t)
+      (setq mac-command-modifier 'meta)
+      (setq mac-option-modifier nil))))
+
+
 (setq viper-mode t)
 (require 'viper)
 
