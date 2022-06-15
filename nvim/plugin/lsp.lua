28196ca3b2ee5ae4662548c626fd948495576911
Julien Castelain <julien@users.noreply.github.com>
2022-06-15 23:53:49 +0200

    Update lsp settings
    Gets rid of things I don't use
    
    The most important for me are:
    Go to definition
    Go to next error (optionally go to the previous error, but at the end
    of the day with go to next you can cycle through them)
    Populate the quickfix list (doesn't update as you type which is
    annoying)
    List implementations

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -3,12 +3,9 @@ local on_attach = function(client, bufnr)
 	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 	local opts = {noremap=true, silent=true}
 	vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	vim.api.nvim_set_keymap('n', '<leader>l', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 	vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 	vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-	vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-	vim.api.nvim_set_keymap('n', '<leader>D', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts)
+	vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 end
 
