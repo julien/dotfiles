dca29fa33b104e58ee4775e351cdeeff8caaaba6
Julien Castelain <julien@users.noreply.github.com>
2022-09-14 14:54:42 +0200

    Add mapping for lsp references

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -9,6 +9,7 @@ local on_attach = function(client, bufnr)
 	vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts)
 	vim.api.nvim_set_keymap('n', '<leader>v', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 end
 
 for _, lsp in ipairs({'gopls', 'rust_analyzer'}) do
